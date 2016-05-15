5897d4c931f369b12e8962e85bdb31ce4eb4417a
Kawisara Issama-ael <Kawisara Issama-ael>
2016-05-15 10:48:25 +0700

    home

--- index.php
+++ index.php
@@ -7,9 +7,34 @@
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
 </head>
-<body>
-	Hello World
-
+<body>	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-1 bg-primary">logo</div>
+			<div class="col-md-9 bg-danger">
+				<div id="custom-search-input">
+					<div class="input-group col-md-12">
+					<input type="text" class="form-control input-lg" placeholder="Search" />
+						<span class="input-group-btn">
+							<button class="btn btn-info btn-lg" type="button">
+								<i class="glyphicon glyphicon-search"></i>
+							</button>
+						</span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-1 bg-primary">
+				sign in
+			</div>
+			<div class="col-md-1 bg-danger">
+				sign up			
+			</div>		
+		</div>
+		<div class="row">
+			<div class="col-md-1 bg-danger">menu</div>
+			<div class="col-md-11 bg-primary">shop</div>
+		</div>
+	</div>
 	<script src="jquery.min.js"></script>
 	<script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
