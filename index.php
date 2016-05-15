546362f244c14cfd4973edc45817e90ccb39b35c
Kawisara Issama-ael <Kawisara Issama-ael>
2016-05-15 11:19:58 +0700

    menu

--- index.php
+++ index.php
@@ -10,18 +10,20 @@
 <body>	
 	<div class="container">
 		<div class="row">
-			<div class="col-md-1 bg-primary">logo</div>
-			<div class="col-md-9 bg-danger">
-				<div id="custom-search-input">
-					<div class="input-group col-md-12">
-					<input type="text" class="form-control input-lg" placeholder="Search" />
-						<span class="input-group-btn">
-							<button class="btn btn-info btn-lg" type="button">
-								<i class="glyphicon glyphicon-search"></i>
-							</button>
-						</span>
+			<div class="col-md-2 bg-primary">logo</div>		
+			<div class="col-md-8 bg-danger">
+				<form action="#" method="get" accept-charset="utf-8">
+					<div id="custom-search-input">
+						<div class="input-group col-md-12">
+							<input type="text" class="form-control input-lg" placeholder="Search" />
+							<span class="input-group-btn">
+								<button class="btn btn-info btn-lg" type="button">
+									<i class="glyphicon glyphicon-search"></i>
+								</button>
+							</span>
+						</div>
 					</div>
-				</div>
+				</form>
 			</div>
 			<div class="col-md-1 bg-primary">
 				sign in
@@ -31,11 +33,22 @@
 			</div>		
 		</div>
 		<div class="row">
-			<div class="col-md-1 bg-danger">menu</div>
-			<div class="col-md-11 bg-primary">shop</div>
-		</div>
+			<div class="col-md-2 bg-danger">
+				<div>
+					<label>menu</label>
+				</div>
+				<div><a href="" title=""><button type="button" class="btn-primary">อาหารแมว</button></a></div>
+				<div><a href="" title=""><button type="button" class="btn-primary">อาหารแมว</button></a></div>
+				<div><a href="" title=""><button type="button" class="btn-primary">อาหารแมว</button></a></div>
+				<div><a href="" title=""><button type="button" class="btn-primary">อาหารแมว</button></a></div>
+				<div><a href="" title=""><button type="button" class="btn-primary">อาหารแมว</button></a></div>
+				<div><a href="" title=""><button type="button" class="btn-primary">อาหารแมว</button></a></div>
+			</div>
+			<div class="col-md-10 bg-primary">shop</div>	
+		</div>	
 	</div>
-	<script src="jquery.min.js"></script>
-	<script src="bootstrap/js/bootstrap.min.js"></script>
+</div>
+<script src="jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
