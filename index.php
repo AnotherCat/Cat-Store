01755b7917713fe2325a5128f8fd19f33950442c
Kawisara Issama-ael <Kawisara Issama-ael>
2016-05-23 01:04:53 +0700

    index

--- index.php
+++ index.php
@@ -30,147 +30,251 @@ if(isset($_SESSION["profiles"])){
 			<!--menu-->
 			<div class="col-md-2 menu">
 				<ul class="nav nav-pills nav-stacked">
-					<li class="active"><a href="#">Cat Store</a></li>
 					<li><a href="#"><img src="pictures/cat-logo-food.png" alt="" width="50" height="50">อาหารแมว</a></li>
 					<li><a href="#"><img src="pictures/cat-logo-toy.png" alt="" width="50" height="50">ของเล่น</a></li>
 					<li><a href="#"><img src="pictures/cat-logo-pill.png" alt="" width="50" height="50">ยารักษาโรค</a></li>
+					<li><a href="#"><img src="pictures/cat-logo-sand.png" alt="" width="50" height="50">ทรายแมว</a></li>
 				</ul>
 			</div>
 
 			<!--shop-->
 			<div class="col-md-10 highLight">
-				<div id="myCarousel" class="carousel slide" data-ride="carousel">
-					<!-- Indicators -->
-					<ol class="carousel-indicators">
-						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-						<li data-target="#myCarousel" data-slide-to="1"></li>
-						<li data-target="#myCarousel" data-slide-to="2"></li>
-						<li data-target="#myCarousel" data-slide-to="3"></li>
-						<li data-target="#myCarousel" data-slide-to="4"></li>
-					</ol>
-
-					<!-- Wrapper for slides -->
-					<div class="carousel-inner" role="listbox">
-						<div class="item active">
-							<img src="pictures/catfood.jpg" class="center-block lo-pd">
-							<div class="carousel-caption">
-								<h3>Me-O Persian Anti hairball formula</h3>
-								<p>อาหารแมวชนิดเม็ด สำหรับแมวเปอร์เซีย สูตรป้องกันก้อนขนอุดตัน</p>
-							</div>
-						</div>
-						<div class="item">
-							<img src="pictures/catfood2.jpg" class="center-block lo-pd">
-							<div class="carousel-caption">
-								<h3>Me-O Seafood</h3>
-								<p>อาหารแมวชนิดเม็ด สำหรับแมวทุกสายพันธุ์ อายุตั้งแต่ 1 ปีขึ้นไป สูตรซีฟู้ด</p>
-							</div>
-						</div>
-						<div class="item">
-							<img src="pictures/catfood3.jpg" class="center-block lo-pd">
-							<div class="carousel-caption">
-								<h3>Royal Canin Kitten Persain32</h3>
-								<p>อาหารแมวชนิดเม็ด สูตรเฉพาะสำหรับลูกแมวเปอร์เซีย</p>
-							</div>
-						</div>
-						<div class="item">
-							<img src="pictures/catfood4.jpg" class="center-block lo-pd">
-							<div class="carousel-caption">
-								<h3>Royal Canin Babycat</h3>
-								<p>อาหารแมวชนิดเม็ด สำหรับแม่แมวเปอร์เซีย</p>
-							</div>
-						</div>
-						<div class="item">
-							<img src="pictures/catfood5.jpg" class="center-block lo-pd">
-							<div class="carousel-caption">
-								<h3>Royal Canin GASTRO INTESTINAL</h3>
-								<p>อาหารประกอบการรักษาโรคระบบทางเดินอาหาร สำหรับแมวที่มีปัญหาท้องเสีย</p>
-							</div>
-						</div>
-					</div>
-
-					<!-- Left and right controls -->
-					<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-						<span class="sr-only">Previous</span>
-					</a>
-					<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-						<span class="sr-only">Next</span>
-					</a>
-				</div>
+				
+				<h1 class="text-center">ยินดีต้อนรับสู่ ร้าน cat-store</h1>
 			</div>	
 		</div>	
-		<div class="row">
-			<!--shop-->
-			<div class="col-md-10 col-md-offset-2 highLight">
-				<div id="myCarousel1" class="carousel slide" data-ride="carousel">
-					<!-- Indicators -->
-					<ol class="carousel-indicators">
-						<li data-target="#myCarousel1" data-slide-to="0" class="active"></li>
-						<li data-target="#myCarousel1" data-slide-to="1"></li>
-						<li data-target="#myCarousel1" data-slide-to="2"></li>
-						<li data-target="#myCarousel1" data-slide-to="3"></li>
-						<li data-target="#myCarousel1" data-slide-to="4"></li>
-					</ol>
+		<!--cat food&toys show-->
+		<div class="row slice">
+			<div class="row">
+				<div class="col-md-2 bg">
+					<p class="text-center">CAT FOODS</p>
+				</div>	
+				<div class="col-md-2 col-md-offset-4 bg">
+					<p class="text-center">CAT TOYS</p>
+				</div>	
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div id="myCarousel-food" class="carousel slide" data-ride="carousel">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#myCarousel-food" data-slide-to="0" class="active"></li>
+							<li data-target="#myCarousel-food" data-slide-to="1"></li>
+							<li data-target="#myCarousel-food" data-slide-to="2"></li>
+							<li data-target="#myCarousel-food" data-slide-to="3"></li>
+							<li data-target="#myCarousel-food" data-slide-to="4"></li>
+						</ol>
 
-					<!-- Wrapper for slides -->
-					<div class="carousel-inner" role="listbox">
-						<div class="item active">
-							<img src="pictures/catfood.jpg" class="center-block lo-pd">
-							<div class="carousel-caption">
-								<h3>Me-O Persian Anti hairball formula</h3>
-								<p>อาหารแมวชนิดเม็ด สำหรับแมวเปอร์เซีย สูตรป้องกันก้อนขนอุดตัน</p>
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner" role="listbox">
+							<div class="item active">
+								<img src="pictures/catfood.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Me-O Persian Anti hairball</h3>						
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/catfood2.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Me-O Seafood</h3>
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/catfood3.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Royal Canin Kitten Persain32</h3>
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/catfood4.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Royal Canin Babycat</h3>
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/catfood5.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Royal Canin GASTRO INTESTINAL</h3>
+								</div>
 							</div>
 						</div>
-						<div class="item">
-							<img src="pictures/catfood2.jpg" class="center-block lo-pd">
-							<div class="carousel-caption">
-								<h3>Me-O Seafood</h3>
-								<p>อาหารแมวชนิดเม็ด สำหรับแมวทุกสายพันธุ์ อายุตั้งแต่ 1 ปีขึ้นไป สูตรซีฟู้ด</p>
-							</div>
-						</div>
-						<div class="item">
-							<img src="pictures/catfood3.jpg" class="center-block lo-pd">
-							<div class="carousel-caption">
-								<h3>Royal Canin Kitten Persain32</h3>
-								<p>อาหารแมวชนิดเม็ด สูตรเฉพาะสำหรับลูกแมวเปอร์เซีย</p>
-							</div>
-						</div>
-						<div class="item">
-							<img src="pictures/catfood4.jpg" class="center-block lo-pd">
-							<div class="carousel-caption">
-								<h3>Royal Canin Babycat</h3>
-								<p>อาหารแมวชนิดเม็ด สำหรับแม่แมวเปอร์เซีย</p>
-							</div>
-						</div>
-						<div class="item">
-							<img src="pictures/catfood5.jpg" class="center-block lo-pd">
-							<div class="carousel-caption">
-								<h3>Royal Canin GASTRO INTESTINAL</h3>
-								<p>อาหารประกอบการรักษาโรคระบบทางเดินอาหาร สำหรับแมวที่มีปัญหาท้องเสีย</p>
-							</div>
-						</div>
+
+						<!-- Left and right controls -->
+						<a class="left carousel-control" href="#myCarousel-food" role="button" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#myCarousel-food" role="button" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
 					</div>
+				</div>
+				<div class="col-md-6">
+					<div id="myCarousel-toy" class="carousel slide" data-ride="carousel">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#myCarousel-toy" data-slide-to="0" class="active"></li>
+							<li data-target="#myCarousel-toy" data-slide-to="1"></li>
+							<li data-target="#myCarousel-toy" data-slide-to="2"></li>
+							<li data-target="#myCarousel-toy" data-slide-to="3"></li>
+						</ol>
 
-					<!-- Left and right controls -->
-					<a class="left carousel-control" href="#myCarousel1" role="button" data-slide="prev">
-						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-						<span class="sr-only">Previous</span>
-					</a>
-					<a class="right carousel-control" href="#myCarousel1" role="button" data-slide="next">
-						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-						<span class="sr-only">Next</span>
-					</a>
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner" role="listbox">
+							<div class="item active">
+								<img src="pictures/4h.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Cat Toys for Entrancing</h3>
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/2h.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Cat Mouse Toy</h3>
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/3h.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Cat Toys for Artistic</h3>
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/1h.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Cat Condo</h3>
+								</div>
+							</div>
+						</div>
+
+						<!-- Left and right controls -->
+						<a class="left carousel-control" href="#myCarousel-toy" role="button" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#myCarousel-toy" role="button" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
 				</div>
 			</div>	
-
-			
 		</div>
+		<!--end Catfood&toys show-->
 
+		<!--cat pill&sands show-->
+		<div class="row slice">
+			<div class="row">
+				<div class="col-md-2 bg">
+					<p class="text-center">HEALTH&CARE</p>
+				</div>	
+				<div class="col-md-2 col-md-offset-4 bg">
+					<p class="text-center">CAT LITTER</p>
+				</div>	
+			</div>
+			<div class="row">
+				<div class="col-md-6">
+					<div id="myCarousel-hc" class="carousel slide" data-ride="carousel">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#myCarousel-hc" data-slide-to="0" class="active"></li>
+							<li data-target="#myCarousel-hc" data-slide-to="1"></li>
+							<li data-target="#myCarousel-hc" data-slide-to="2"></li>
+							<li data-target="#myCarousel-hc" data-slide-to="3"></li>
+						</ol>
 
-	</div>
-	
-	<script src="jquery.min.js"></script>
-	<script src="bootstrap/js/bootstrap.min.js"></script>
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner" role="listbox">
+							<div class="item active">
+								<img src="pictures/c1.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>FRONTLINE Plus</h3>				
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/c2.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>FRONTLINE Spay</h3>		
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/c3.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>HOBBYY</h3>		
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/c4.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Pet Grass</h3>	
+								</div>
+							</div>
+						</div>
+
+						<!-- Left and right controls -->
+						<a class="left carousel-control" href="#myCarousel-hc" role="button" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#myCarousel-hc" role="button" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div id="myCarousel-lit" class="carousel slide" data-ride="carousel">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#myCarousel-lit" data-slide-to="0" class="active"></li>
+							<li data-target="#myCarousel-lit" data-slide-to="1"></li>
+							<li data-target="#myCarousel-lit" data-slide-to="2"></li>
+						</ol>
+
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner" role="listbox">
+							<div class="item active">
+								<img src="pictures/5952079.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>CATLUX</h3>
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/cat_litter_.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Cat Litter Home Band</h3>
+								</div>
+							</div>
+							<div class="item">
+								<img src="pictures/pristinep28catlitterscoop.jpg" class="center-block lo-pd">
+								<div class="carousel-caption">
+									<h3>Pristine Paws</h3>
+								</div>
+							</div>
+						</div>
+
+						<!-- Left and right controls -->
+						<a class="left carousel-control" href="#myCarousel-lit" role="button" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#myCarousel-lit" role="button" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+			</div>	
+		</div>
+		<!--end CatPill&Sands show-->	
+
+		
+	</div>	
+</div>
+</div>
+
+<script src="jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
